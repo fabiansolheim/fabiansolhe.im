7ce3ff2b03322e747fa7d7a8099827aaf0224c2d
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-01-20 20:52:12 +0100

    removed some boilerplate

--- src/App.js
+++ src/App.js
@@ -18,21 +18,6 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
         </Grid>
       </Box>
     </ChakraProvider>
