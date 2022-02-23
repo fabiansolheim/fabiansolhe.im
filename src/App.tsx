06426fc61f918bb8351420973d05254e2568b703
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-02-23 10:13:14 +0100

    migrated to typescript

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import React, {useEffect} from 'react';
+import {Link as RouterLink} from 'react-router-dom';
+import {
+  ChakraProvider,
+  HStack,
+  Box,
+  Text,
+  Link,
+  Container,
+  Image,
+  Button,
+  VStack,
+  Code,
+  Center,
+  Heading,
+  Grid,
+  theme,
+  useMediaQuery,
+  Flex,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './components/styling/ColorModeSwitcher';
+import Footer from './components/Footer';
+import ToggleColorMode from './components/ToggleColorMode';
+import './components/styling/wave.css';
+import container from './components/styling/framerAnimations';
+import { motion } from 'framer-motion';
+
+/* const HeroText = () => {
+  return(
+    <Box w={[300, 400, 500]} mt={60}>
+    <Heading mb={4}>Hei
+    <span className="wave">👋🏼</span>
+    </Heading>
+    <HStack>
+    <Text fontSize='xl' style={{display: 'inline-block'}}>
+      Jeg er <Text style={{fontWeight: 'bold', display: 'inline-block'}}>Fabian Solheim</Text>. Jeg er en 21 år gammel student, og utvikler.
+    </Text>
+    </HStack>
+  </Box>
+  )
+}
+
+ */
+
+const HeroText = () => {
+  const [isNotMobile] = useMediaQuery('(min-width: 600px)');
+  return (
+    <>
+
+    <Box mt={isNotMobile ? 60 : 153} position="relative" w={[300, 400, 500]}>
+      <Heading mb={4}>
+        Hei
+        <span className="wave">👋🏼</span>
+      </Heading>
+      <HStack>
+        <Text fontSize="xl" style={{ display: 'inline-block' }}>
+          Jeg er{' '}
+          <Text style={{ fontWeight: 'bold', display: 'inline-block' }}>
+            Fabian Solheim
+          </Text>
+          . Jeg er en 21 år gammel informatikkstudent, og utvikler.
+        </Text>
+      </HStack>
+    </Box>
+
+<Box w={390} mt={10}>
+<Text fontSize="xl">
+  Ta gjerne en titt på{' '}
+  <Link
+    as={RouterLink}
+    to="/prosjekter"
+    style={{ textDecoration: 'underline' }}
+  >
+    mine prosjekter
+  </Link>
+  , min{' '}
+  <Link
+    href="https://www.linkedin.com/in/FabianSolheim"
+    style={{ textDecoration: 'underline' }}
+  >
+    LinkedIn
+  </Link>
+  , eller min{' '}
+  <Link
+    href="https://www.github.com/FabianSolheim"
+    style={{ textDecoration: 'underline' }}
+  >
+    Github
+  </Link>
+  .
+</Text>
+</Box>
+
+</>
+  );
+};
+
+const Index = () => {
+  const [isNotMobile] = useMediaQuery('(min-width: 600px)');
+  useEffect(() => {
+    document.title = "👋🏼 Hjem | fabiansolhe.im";
+  }, []);
+
+  return (
+    <>
+           <motion.div
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={container}
+          >    
+      <Container maxWidth="container.lg" minHeight="69.7vh">
+        <Box ml={isNotMobile ? '0' : 5}>
+          <HeroText />
+        </Box>
+      </Container>
+      </motion.div>
+    </>
+  );
+};
+export default Index;
