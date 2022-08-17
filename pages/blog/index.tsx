c8235ba1ec3537ecc05a10bae79afcfc8f7f016a
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-08-17 15:49:29 +0200

    Small change

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -34,15 +34,14 @@ const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
   return (
     <>
-      <Flex
-        mt={50}
-        justifyContent="space-between"
-        gap={6}
-      >
+      <Grid mt={50} 
+      w={"100%"}
+      gap={10}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}>
         {posts.map(({ slug, frontmatter }: Post) => (
          <BlogListPreview key={slug} slug={slug} frontmatter={frontmatter} />
         ))}
-      </Flex>
+      </Grid>
     </>
   );
 };
@@ -52,14 +51,17 @@ const date = parseISO(frontmatter.date);
 const formatted = format(date, "LLLL d, yyyy");
 
   return (
-    <Box key={slug}>
+    <Box key={slug} w={{ base: '100%'}}  border="2px green solid">
       <Link href={`/blog/post/${slug}`}>
         <a>
           <Image
             src={frontmatter.socialImage}
             alt={frontmatter.title}
-            width={300}
-            height={200}
+            width={"100%"}
+            height={"40%"}
+            objectFit="cover"
+            overflow="hidden"
+            borderRadius={8}
           />
           <Box bg="white.500" mt={5}>
             <Text>{formatted}</Text>
